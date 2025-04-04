cb95b73e729e17caef2a720e461ffd10a3d12261
shahadathhs <shahadathhossensajib732@gmail.com>
2025-04-04 08:47:57 +0600

    chore: added express eslint config

--- apps/express-rest-api/eslint.config.js
+++ apps/express-rest-api/eslint.config.js
@@ -1,32 +1,4 @@
-import pluginJs from '@eslint/js'
-import importPlugin from 'eslint-plugin-import'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import { expressConfig } from '@workspace/eslint-config/express'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { ignores: ['node_modules/**', 'dist/**'] },
-  { languageOptions: { globals: globals.node } },
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'no-unused-expressions': 'error',
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          'newlines-between': 'always',
-          alphabetize: { order: 'asc', caseInsensitive: true },
-        },
-      ],
-      // 'import/no-unresolved': 'error'
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-]
+/** @type {import("eslint").Linter.Config} */
+export default expressConfig
